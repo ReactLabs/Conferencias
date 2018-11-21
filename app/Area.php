d980a3133a525e5dca1d2752b2d5cb3808c83d36
geovanneoliveira <geovanneoliveira3@gmail.com>
2018-11-21 09:04:11 -0300

    fillables + pivo (construct)

--- app/Area.php
+++ app/Area.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Area extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function events(){
+        return $this->belongsToMany(Event::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
 }
--- app/Event.php
+++ app/Event.php
@@ -17,6 +17,14 @@ class Event extends Model
         'name','initials','date','description','qualis','link','deadline','user_id',
     ];
 
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function areas(){
+        return $this->belongsToMany(Area::class);
+    }
+
     public function getUserName(){
         $user = User::find($this->user_id);
         return $user->name;
