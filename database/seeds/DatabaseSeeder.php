850d0a044b5d0c7966591ac2ff09f0467573fc99
Thomás <thomastabosa10@gmail.com>
2018-12-06 18:36:11 -0300

    seed complete

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\User;
 use App\Event;
+use App\Area;
+use App\Tag;
 
 
 class DatabaseSeeder extends Seeder
@@ -24,7 +26,20 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UsersTableSeeder::class);
 
+        Area::create([
+            'name' => 'si'
+        ]);
+
+        $area = Area::where('name', '=', 'si')->first();
+        Tag::create([
+            'name' => 'banco de dados',
+            'area_id' => $area->id
+        ]);
+
+
         $user = User::where('name', '=', 'geovanne')->first();
+        $area = Area::where('name', '=', 'si')->first();
+        $tag = Tag::where('area_id', '=', $area->id)->first();
         Event::create([
             'name' => 'Jornada de Ciência e Extensão',
             'initials' => 'JCE',
@@ -35,15 +50,9 @@ class DatabaseSeeder extends Seeder
             'deadline' => '2018-10-21',
             'user_id' => $user->id,
         ]);
-        Event::create([
-            'name' => 'Evento qualquer',
-            'initials' => 'Evt',
-            'date' => '2018-12-31',
-            'description' => 'Um evento qualquer',
-            'qualis' => 'qualis',
-            'link' => 'link',
-            'deadline' => '2018-11-15',
-            'user_id' => $user->id,
-        ]);
+
+        $event = Event::where('name', '=', 'Jornada de Ciência e Extensão')->first();
+        $event->areas()->attach($area);
+        $event->tags()->attach($tag);
     }
 }
