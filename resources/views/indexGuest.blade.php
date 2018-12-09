71dfd9916500da666706ecd2871cf1d63529d457
geovanneoliveira <geovanneoliveira3@gmail.com>
2018-12-09 09:54:02 -0300

    alter view Grid + search advanced

--- resources/views/indexGuest.blade.php
+++ resources/views/indexGuest.blade.php
@@ -3,127 +3,141 @@
 
 
 
-<div class="container">
-	<div class="row justify-content-center">
-		<div class="col-md-8">
-			<br/>
-			@if (\Session::has('success'))
-			<div class="alert alert-info alert-dismissible fade show text-center">
-				<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
-				<strong>{{ \Session::get('success') }}</strong>
-			</div>
-			<br/>
-			@endif
-			@if (\Session::has('warning'))
-			<div class="alert alert-warning alert-dismissible fade show text-center">
-				<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
-				<strong>{{ \Session::get('warning') }}</strong>
-			</div>
-			<br/>
-			@endif
-			<div class="card">
-				<div class="card-header">Events</div>
-				<div class="card-body table-responsive">
-					@if (session('status'))
-					<div class="alert alert-success" role="alert">
-						{{ session('status') }}
-					</div>
-					@endif
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <br/>
+                @if (\Session::has('success'))
+                    <div class="alert alert-info alert-dismissible fade show text-center">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
+                        <strong>{{ \Session::get('success') }}</strong>
+                    </div>
+                    <br/>
+                @endif
+                @if (\Session::has('warning'))
+                    <div class="alert alert-warning alert-dismissible fade show text-center">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
+                        <strong>{{ \Session::get('warning') }}</strong>
+                    </div>
+                    <br/>
+                @endif
+                <div class="card">
+                    <div class="card-header">Events</div>
+                    <div class="card-body table-responsive">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-					<div class="card">
-	
-						<div class="card-body">
+                        <div class="card-body">
 
-							<label class="col-md-12 col-form-label text-md-center">{{ __('Advanced Search') }}</label><br>
+                            <table id="filtroTabelaGuest" class="table table-striped text-center">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Initials</th>
+                                    <th>Deadline</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($events as $event)
+                                    <tr>
+                                        <td>{{ $event->id }}</td>
+                                        <td><a href="{{action('GuestController@show', $event->id)}}">{{ $event->name }}
+                                        </td>
+                                        <td>{{ $event->initials }}</td>
+                                        <td>{{ $event->deadline }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-							<form method="GET" action="{{ action('EventController@eventsFilter') }}">
-								@csrf
+            <div class="col-md-4">
+                <br>
+                <div class="card">
+                    <div class="card-header">Advanced Search</div>
+                    <div class="card-body">
 
-								<div class="form-group row">
-									<label for="area" class="col-md-4 col-form-label text-md-right">{{ __('Area') }}</label>
-		
-									<div class="col-md-6">
-										<select id="area" class="form-control selectpicker" data-live-search="true" name="area">
-											<option value="0">Choose the area...</option>
-											<option data-divider="true"></option>
-											@foreach($areas as  $area)
-												<option value="{{ $area->id }}">{{ $area->name }}</option>
-											@endforeach
-										</select>
-									</div>
-								</div>
+                    <!--<label class="col-md-12 col-form-label text-md-center">{{ __('Advanced Search') }}</label><br>-->
 
-								<div class="form-group row">
-									<label for="date" class="col-md-4 col-form-label text-md-right">{{ __('Date from') }}</label>
-		
-									<div class="col-md-6">
+                        <form method="GET" action="{{ action('EventController@eventsFilter') }}">
+                            @csrf
 
-										<input id="date_from" type="date" class="form-control" name="date_from" value="{{  old('date_from') }}">
+                            <div class="form-group row">
+                                <label for="area"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Area') }}</label>
 
-									</div>
-								</div>
+                                <div class="col-md-6">
+                                    <select id="area" class="form-control selectpicker" data-live-search="true"
+                                            name="area">
+                                        <option value="0">Choose the area...</option>
+                                        <option data-divider="true"></option>
+                                        @foreach($areas as  $area)
+                                            <option value="{{ $area->id }}">{{ $area->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
 
-								<div class="form-group row">
-									<label for="date" class="col-md-4 col-form-label text-md-right">{{ __('Date to') }}</label>
-		
-									<div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="date"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Date from') }}</label>
 
-										<input id="date_to" type="date" class="form-control" name="date_to" value="{{  old('date_to') }}">
-									
-									</div>
-								</div>
-		
-								<div class="form-group row mb-0">
-									<div class="col-md-6 offset-md-4">
-										<button type="submit" class="btn btn-primary">
-											{{ __('Search') }}
-										</button>
-									</div>
-								</div>
-							</form>
-						</div>
-					</div>
-					<br><br>
+                                <div class="col-md-6">
 
-					<table id="filtroTabelaGuest" class="table table-striped text-center">
-						<thead>
-							<tr>
-								<th>ID</th>
-								<th>Name</th>
-								<th>Initials</th>
-								<th>Deadline</th>
-							</tr>
-						</thead>
-						<tbody>
-							@foreach ($events as $event)
-								<tr>
-									<td>{{ $event->id }}</td>
-									<td><a href="{{action('GuestController@show', $event->id)}}">{{ $event->name }}</td>
-									<td>{{ $event->initials }}</td>
-									<td>{{ $event->deadline }}</td>
-								</tr>
-							@endforeach
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                                    <input id="date_from" type="date" class="form-control" name="date_from"
+                                           value="{{  old('date_from') }}">
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
+                                </div>
+                            </div>
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
+                            <div class="form-group row">
+                                <label for="date"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Date to') }}</label>
 
-<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+                                <div class="col-md-6">
 
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+                                    <input id="date_to" type="date" class="form-control" name="date_to"
+                                           value="{{  old('date_to') }}">
 
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+                                </div>
+                            </div>
 
-<script src="{{ asset('js/datatable.js') }}"></script>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Search') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
+
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+
+    <script type="text/javascript" charset="utf8"
+            src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+    <script src="{{ asset('js/datatable.js') }}"></script>
 
 
 @endsection
--- resources/views/moderator/event/index.blade.php
+++ resources/views/moderator/event/index.blade.php
@@ -3,143 +3,160 @@
 
 
 
-<div class="container">
-	<div class="row justify-content-center">
-		<div class="col-md-8">
-			<br/>
-			@if (\Session::has('success'))
-			<div class="alert alert-info alert-dismissible fade show text-center">
-				<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
-				<strong>{{ \Session::get('success') }}</strong>
-			</div>
-			<br/>
-			@endif
-			@if (\Session::has('warning'))
-			<div class="alert alert-warning alert-dismissible fade show text-center">
-				<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
-				<strong>{{ \Session::get('warning') }}</strong>
-			</div>
-			<br/>
-			@endif
-			<div class="card">
-				<div class="card-header">Events</div>
-				<div class="card-body table-responsive">
-					@if (session('status'))
-					<div class="alert alert-success" role="alert">
-						{{ session('status') }}
-					</div>
-					@endif
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <br/>
+                @if (\Session::has('success'))
+                    <div class="alert alert-info alert-dismissible fade show text-center">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
+                        <strong>{{ \Session::get('success') }}</strong>
+                    </div>
+                    <br/>
+                @endif
+                @if (\Session::has('warning'))
+                    <div class="alert alert-warning alert-dismissible fade show text-center">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
+                        <strong>{{ \Session::get('warning') }}</strong>
+                    </div>
+                    <br/>
+                @endif
+                <div class="card">
+                    <div class="card-header">Events</div>
+                    <div class="card-body table-responsive">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-					<div class="card">
-		
-						<div class="card-body">
+                        <div class="card-body">
 
-							<label class="col-md-12 col-form-label text-md-center">{{ __('Advanced Search') }}</label><br>
+                            <table id="filtroTabela" class="table table-striped text-center">
+                                <thead>
+                                <tr>
+                                    <th rowspan="2">ID</th>
+                                    <th rowspan="2">Name</th>
+                                    <th rowspan="2">Initials</th>
+                                    <th rowspan="2">Deadline</th>
+                                    <th colspan="2">Operation</th>
+                                </tr>
+                                <tr>
+                                    <!-- Colunas para colocar os botões na mesma coluna-->
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($events as $event)
+                                    <tr>
+                                        <td>{{ $event->id }}</td>
+                                        <td><a href="{{action('EventController@show', $event->id)}}">{{ $event->name }}
+                                        </td>
+                                        <td>{{ $event->initials }}</td>
+                                        <td>{{ $event->deadline }}</td>
+                                        <td><a href="{{action('EventController@edit', $event->id)}}"
+                                               class="btn btn-warning"> Edit </a></td>
+                                        <td>
+                                            <form action="{{ action('EventController@destroy', $event->id) }}"
+                                                  method="post">
+                                                @csrf
+                                                <input name="_method" type="hidden" value="DELETE">
+                                                <button class="btn btn-danger" type="submit">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-							<form method="GET" action="{{ action('EventController@eventsFilter') }}">
-								@csrf
+            <div class="col-md-4">
+                <br>
+                <div class="card">
+                    <div class="card-header">Advanced Search</div>
+                    <div class="card-body">
 
-								<div class="form-group row">
-									<label for="area" class="col-md-4 col-form-label text-md-right">{{ __('Area') }}</label>
-		
-									<div class="col-md-6">
-										<select id="area" class="form-control selectpicker" data-live-search="true" name="area">
-											<option value="0">Choose the area...</option>
-											<option data-divider="true"></option>
-											@foreach($areas as  $area)
-												<option value="{{ $area->id }}">{{ $area->name }}</option>
-											@endforeach
-										</select>
-									</div>
-								</div>
+                    <!-- <label class="col-md-12 col-form-label text-md-center">{{ __('Advanced Search') }}</label><br> -->
 
-								<div class="form-group row">
-									<label for="date" class="col-md-4 col-form-label text-md-right">{{ __('Date from') }}</label>
-		
-									<div class="col-md-6">
+                        <form method="GET" action="{{ action('EventController@eventsFilter') }}">
+                            @csrf
 
-										<input id="date_from" type="date" class="form-control" name="date_from" value="{{  old('date_from') }}">
+                            <div class="form-group row">
+                                <label for="area"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Area') }}</label>
 
-									</div>
-								</div>
+                                <div class="col-md-6">
+                                    <select id="area" class="form-control selectpicker" data-live-search="true"
+                                            name="area">
+                                        <option value="0">Choose the area...</option>
+                                        <option data-divider="true"></option>
+                                        @foreach($areas as  $area)
+                                            <option value="{{ $area->id }}">{{ $area->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
 
-								<div class="form-group row">
-									<label for="date" class="col-md-4 col-form-label text-md-right">{{ __('Date to') }}</label>
-		
-									<div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="date"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Date from') }}</label>
 
-										<input id="date_to" type="date" class="form-control" name="date_to" value="{{  old('date_to') }}">
-									
-									</div>
-								</div>
-		
-								<div class="form-group row mb-0">
-									<div class="col-md-6 offset-md-4">
-										<button type="submit" class="btn btn-primary">
-											{{ __('Search') }}
-										</button>
-									</div>
-								</div>
-							</form>
-						</div>
-					</div>
-					<br><br>
+                                <div class="col-md-6">
 
-					<table id="filtroTabela" class="table table-striped text-center">
-						<thead>
-							<tr>
-								<th rowspan="2">ID</th>
-								<th rowspan="2">Name</th>
-								<th rowspan="2">Initials</th>
-								<th rowspan="2">Deadline</th>
-								<th colspan="2">Operation</th>
-							</tr>
-							<tr>
-								<!-- Colunas para colocar os botões na mesma coluna-->
-								<th></th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							@foreach ($events as $event)
-								<tr>
-									<td>{{ $event->id }}</td>
-									<td><a href="{{action('EventController@show', $event->id)}}">{{ $event->name }}</td>
-									<td>{{ $event->initials }}</td>
-									<td>{{ $event->deadline }}</td>
-									<td> <a href="{{action('EventController@edit', $event->id)}}"class="btn btn-warning"> Edit </a></td>
-									<td>
-										<form action="{{ action('EventController@destroy', $event->id) }}" method="post">
-											@csrf
-											<input name="_method" type="hidden" value="DELETE">
-											<button class="btn btn-danger" type="submit">Delete</button>
-										</form>
-									</td>
-								</tr>
-							@endforeach
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                                    <input id="date_from" type="date" class="form-control" name="date_from"
+                                           value="{{  old('date_from') }}">
+
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="date"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Date to') }}</label>
+
+                                <div class="col-md-6">
+
+                                    <input id="date_to" type="date" class="form-control" name="date_to"
+                                           value="{{  old('date_to') }}">
+
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Search') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 
 
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
 
-<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
 
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8"
+            src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
-<script src="{{ asset('js/datatable.js') }}"></script>
+    <script src="{{ asset('js/datatable.js') }}"></script>
 
 
 @endsection
