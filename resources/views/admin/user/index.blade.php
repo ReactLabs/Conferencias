ad8945d5265a923353c1da948be8d32552e97a7b
geovanneoliveira <geovanneoliveira3@gmail.com>
2018-12-22 23:04:09 -0300

    adapter table tag

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -26,7 +26,7 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                        <table class="table table-striped text-center">
+                        <table class="table-striped text-center">
                             <thead>
                             <tr>
                                 <th>ID</th>
@@ -67,9 +67,13 @@
             </div>
         </div>
     </div>
+
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+    <script src="{{ asset('js/datatable.js') }}"></script>
+
 @endsection
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
-
-<script src="{{ asset('js/datatable.js') }}" defer></script>
